3c171b825dfb3127df2393d8dec01115800f7142
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-05-29 15:46:18 -0300

    first props test

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,7 +1,6 @@
-import './App.css'
 import {useState} from 'react'
 
-export function Image() {
+export function Image(props) {
     const [img, setImg] = useState("https://picsum.photos/600")
     const changeImg = () => {  
         setImg("https://picsum.photos/400")
@@ -9,7 +8,7 @@ export function Image() {
     return (
         <>
             <img src={img} className="img-test" alt="" />
-            <button className="button-img" onClick={changeImg}>Change image</button>
+            <button className="button-img" onClick={changeImg}>{props}</button>
         </>
     )
 }
