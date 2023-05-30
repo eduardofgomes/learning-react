bf1a4f055f96e2608821d0c8986b952492979f06
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-05-30 18:37:29 -0300

    master props in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import {Image} from './Image.jsx'
 
 export default function App() {
-  const [count, setCount] = useState(0)
-  const changeButton = () => {
-    count <= 9 ? setCount(count + 1) : alert("You've clicked too many times in this button!")
-  }
   return (
     <>
-      <button onClick={changeButton}>clicked {count}</button>
+      <Image />
     </>
   )
 }
--- src/Image.jsx
+++ src/Image.jsx
@@ -1,14 +1,21 @@
 import {useState} from 'react'
 
-export function Image(props) {
+export function Image() {
     const [img, setImg] = useState("https://picsum.photos/600")
     const changeImg = () => {  
         setImg("https://picsum.photos/400")
     }
+    
     return (
         <>
             <img src={img} className="img-test" alt="" />
-            <button className="button-img" onClick={changeImg}>{props}</button>
+            <Button className="button-img" onClick={changeImg} text="TeSt"></Button>
         </>
     )
+}
+
+const Button = (props) =>{
+    const insertedText = props.text
+    const upperText = insertedText.toUpperCase()
+    return <button>{upperText}</button>
 }
