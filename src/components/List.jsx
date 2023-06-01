f46ae29a187d974c64a6e7fc2c154aaf550ce8f9
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-06-01 10:52:10 -0300

    Manage state using useState

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,29 @@
+import {useState} from 'react';
+
+
 const List = () => {
     let superHeroes = [
         'Batman',
         'Superman',
         'Flash'
     ]
+    const [selected, setSelected] = useState()
+
     return (
         <>
             <h1>Super heroes</h1>
             {superHeroes.length === 0 && <p>No super hero found</p>}
             <ul className="list">
-                {superHeroes.map(hero => <li key={hero}>{hero}</li>)}
+                {superHeroes.map((hero, index) => (
+                    <li
+                        key={hero}   
+                        onClick={() => {
+                            setSelected(index), console.log(selected)
+                        }}
+                    >
+                        {hero}{selected}
+                    </li>
+                    ))}
             </ul>
         </>
     )
